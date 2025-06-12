ba300609fdf97f6f4a51cf617a33592b6a1e7c6a
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-06-12 11:52:18 +0100

    init hive with name of the box and change the SDk and NDK of android

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,3 +4,4 @@ const Color kprimaryColor = Colors.tealAccent;
 const Color ksecondaryColor = Colors.purpleAccent;
 const Color kthirdColor = Colors.orange;
 const Color kforthColor = Colors.white;
+const String kNotesBox = "NotesBox";
